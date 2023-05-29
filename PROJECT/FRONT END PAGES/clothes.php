275b6eb8983734007124a28ef62fe420696ce64d
OMJI SHUKLA <91083571+009os@users.noreply.github.com>
2023-05-29 22:26:00 +0530

    Add files via upload

---
+++ PROJECT/FRONT END PAGES/clothes.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css" type="text/css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>clothes</title>
+</head>
+
+<body style="background-image: url('backSHOP.jpg');
+    width:100%;
+ background-position: center;
+ background-size: cover;
+background-repeat: no-repeat">
+    <center>
+        <header>
+            <h1><b><i>CLOTHES COLLECTION</i></b></h1>
+        </header>
+    </center>
+    <hr>
+    <center>
+        <table class="cal2">
+            <tr>
+                <td><a href="" target="_main"><img class="o11" src="clothes1.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>CAFREE PANT<br>BUT AT INR :- 989<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><a href="" target="_main"><img class="o11" src="clothes2.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>ORANGE FULL TOP<br>BUT AT INR :- 1090<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><a href="" target="_main"><img class="o11" src="clothes3.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>CASUAL GREY LOWER<br>BUT AT INR :- 980<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+
+            </tr>
+            <tr>
+                <td><a href="" target="_main"><img class="o11" src="clothes4.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>BLACK T-SHIRT<br>BUT AT INR :- 789<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><a href="" target="_main"><img class="o11" src="clothes5.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>DAMAGE JEANS<br>BUT AT INR :- 1599<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><a href="" target="_main"><img class="o11" src="clothes6.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>HALF-PANT<br>BUT AT INR :- 699<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+
+            </tr>
+
+        </table>
+    </center>
+    <footer>@omjishukla</footer>
+
+</body>
+
+
+</html>
---
+++ PROJECT/FRONT END PAGES/electronics.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css" type="text/css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>electronics</title>
+</head>
+
+<body style="background-image: url('backSHOP.jpg');
+    width:100%;
+ background-position: center;
+ background-size: cover;
+background-repeat: no-repeat">
+    <center>
+        <header>
+            <h1><b><i>ELECTRONICS COLLECTION</i></b></h1>
+        </header>
+    </center>
+    <hr>
+    <center>
+        <table class="cal2">
+            <tr>
+                <td><a href="" target="_main"><img class="o11" src="elect1.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>AIRPODS<br>BUT AT INR :- 9999<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><a href="" target="_main"><img class="o11" src="elect2.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>I PHONE 12 MINI<br>BUT AT INR :- 55,499<br><I><a href="PAYMENT.php"
+                                        target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><a href="" target="_main"><img class="o11" src="elect3.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>MOUSE<br>BUT AT INR :- 499<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+
+            </tr>
+            <tr>
+                <td><a href="" target="_main"><img class="o11" src="elect4.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>FUFILM CAMERA<br>BUT AT INR :- 9990<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><a href="" target="_main"><img class="o11" src="elect5.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>MAC BOOK AIR<br>BUT AT INR :- 1,09,900<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td><a href="" target="_main"><img class="o11" src="elect6.jpg" width="350px" height="300px"></a><br>
+                    <center><B><I>SONY HRADPHONES<br>BUT AT INR :- 4990<br><I><a href="PAYMENT.php" target="_main">BUY
+                                        NOW</a></I></I></B></center>
+                </td>
+
+            </tr>
+
+        </table>
+    </center>
+    <footer>@omjishukla</footer>
+
+</body>
+
+
+</books
